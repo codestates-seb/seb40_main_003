116823bd626250f37775b8f02f5c94ef17c29581
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-09 17:08:42 +0900

    :zap: Care page에 useEffect, useState 추가

--- client/src/Pages/Care.tsx
+++ client/src/Pages/Care.tsx
@@ -1,26 +1,27 @@
-// import { useState } from "react";
-// import { useEffect } from "react";
-// import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 import { SigTag } from "../Components/GlobalComponents";
 import  CareCard  from "../Components/main/CareCard";
+//import { careType } from "../types/careTypes";
 
-type Props = {}
+//type props = [careType];
 
-// useEffect(() => {
-//   axios.get("https://testserver.com/caring").then(({ data }) => {
-//     console.log(data.caring);
-//     setData(data.caring);
-//     setIsLoading(false);
-// })
-// })
+const Care = (props:any) => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    axios.get("https://testserver.com/caring").then(({ data }) => {
+      console.log(data.caring);
+      setData(data.caring);
+      setIsLoading(false);
+    });
+  }, []);
 
-const Care = (props: Props) => {
-  // const [data, setData] = useState<elemMaps>();
-  // const [isLoading, setIsLoading] = useState(true);
   return (
     <div>
-      <CareCard>Care</CareCard>
+      <CareCard />
       <SigTag>배고파</SigTag>
       <SigTag className="variant2">배고파</SigTag>
       <SigTag className="variant3">배고파</SigTag>
