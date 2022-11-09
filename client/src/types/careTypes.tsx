5cdedc32e63628955ed98f52db9ca5b3bc306167
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-09 17:09:36 +0900

    :zap: types에 careTypes 추가

---
+++ client/src/types/careTypes.tsx
@@ -0,0 +1,39 @@
+type careType = 
+  {
+    name : string,
+    age : number,
+    gender : string,
+    photo : [picture: string],
+    simpleCotent : string,
+    detailContent : string,
+    useNum : number,
+    price : number,
+    extra : string,
+    address : string,
+    plant : [
+        {
+            plantId : number,
+            plantPhoto : [picture: string],
+            plantName : string,
+            plantType : string,
+            plantAge : number
+        }
+    ],
+    expertReview : [
+        {
+            reviewId : number,
+            content : string,
+            writerInfo : {
+                userId : number,
+                userNickname : string
+            }
+        }
+    ],
+    techTag : [
+        {
+            techId : number,
+            name : string
+  }]
+}
+;
+export type {careType}
