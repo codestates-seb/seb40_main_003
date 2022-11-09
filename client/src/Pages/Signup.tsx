3189e92ca0444c4f9d4d6238ca2d6a21c539c058
jobkaehenry <1zzangjun@gmail.com>
2022-11-09 11:16:18 +0900

    set

--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-const Register = (props: Props) => {
+const Signup = (props: Props) => {
   return (
     <div>SignUp</div>
   )
 }
 
-export default Register
+export default Signup
