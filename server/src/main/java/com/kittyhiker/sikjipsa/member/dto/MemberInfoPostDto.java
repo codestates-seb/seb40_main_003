d2834b1079bfb0593fa0f6196a36e65a0d8d5eeb
DasolKang <dasol199@naver.com>
2022-11-10 21:50:17 +0900

    :sparkles: [Feat] Member more infromation

---
+++ server/src/main/java/com/kittyhiker/sikjipsa/member/dto/MemberInfoPostDto.java
@@ -0,0 +1,21 @@
+package com.kittyhiker.sikjipsa.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberInfoPostDto {
+    private String name;
+    @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$")
+    private String phone;
+    private String birth;
+    private Integer gender;
+    private String address;
+}
