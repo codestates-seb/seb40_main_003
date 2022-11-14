13bcd531c513a2f5bfc4fd05ac65902e1019ab9a
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-14 19:49:50 +0900

    :zap: CareDetail에 techTag 추가

--- client/src/Pages/Main/CareDetail.tsx
+++ client/src/Pages/Main/CareDetail.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useInRouterContext, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
-import { ProfileCard, CardWrapper, SigTag, ViewCounter } from "../../Components/GlobalComponents";
+import { ProfileCard, CardWrapper, SigTag, ViewCounter, TagWrapper } from "../../Components/GlobalComponents";
 import { userState } from "../../Recoil/atoms/atom";
 import { CareDetailTypes } from "../../types/CareDetailTypes";
 
@@ -40,7 +40,14 @@ const isLogin = useRecoilValue(userState)
             <p>{data.detailContent}</p>
         <CardWrapper><h1 className="h4 bold">반려식물</h1></CardWrapper>    
             {/* 반려식물 컴포넌트 */}
-        <CardWrapper><h1 className="h4 bold">보유 기술</h1></CardWrapper>    
+        <CardWrapper><h1 className="h4 bold">보유 기술</h1></CardWrapper>
+        <TagWrapper>
+        <SigTag>{data.techTag[0].techTagName}</SigTag>   
+            <SigTag>{data.techTag[0].techTagName}</SigTag>   
+            <SigTag>{data.techTag[0].techTagName}</SigTag>   
+            <SigTag>{data.techTag[0].techTagName}</SigTag>   
+            <SigTag>{data.techTag[0].techTagName}</SigTag>   
+        </TagWrapper>
         <CardWrapper><h1 className="h4 bold">소개합니다</h1></CardWrapper>    
         <CardWrapper><h1 className="h4 bold">기본 비용</h1></CardWrapper>    
         <CardWrapper><h1 className="h4 bold">추가 비용</h1></CardWrapper>    
