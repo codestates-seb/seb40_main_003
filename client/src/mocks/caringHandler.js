fa2077a4d7a8e03129f98325de23860989dfe26b
Jungu Lee <1zzangjun@gmail.com>
2022-11-14 15:13:27 +0900

    :zap: this is for you yoon

--- client/src/mocks/caringHandler.js
+++ client/src/mocks/caringHandler.js
@@ -47,11 +47,9 @@ export const CaringListHandler = () => {
                 },
               ],
             },
-          ],
-        },
-        {
-          data: [
             {
+              view: 123,
+              like: 56,
               expertProfileId: 1,
               simpleContent: "식물을 사랑하는 우리 동네 식물 전문가입니다~",
               address: "연남동",
@@ -65,7 +63,7 @@ export const CaringListHandler = () => {
                 },
                 name: "김고수",
                 age: 25,
-                gender: 2,
+                gender: 1,
               },
 
               techTag: [
@@ -82,6 +80,48 @@ export const CaringListHandler = () => {
                   techTagName: "병/해충",
                 },
               ],
+
+              areaTag: [
+                {
+                  areaTagId: 1,
+                  ateaTagName: "마포구",
+                },
+              ],
+            },
+            {
+              view: 123,
+              like: 56,
+              expertProfileId: 1,
+              simpleContent: "식물을 사랑하는 우리 동네 식물 전문가입니다~",
+              address: "연남동",
+              userLikeExpert: 1,
+              member: {
+                memberId: 1,
+                image: {
+                  imageId: 1,
+                  imgUrl: "https://placebear.com/640/360",
+                  isRepImg: 1,
+                },
+                name: "김고수",
+                age: 25,
+                gender: 1,
+              },
+
+              techTag: [
+                {
+                  techTagId: 1,
+                  techTagName: "분갈이",
+                },
+                {
+                  techTagId: 2,
+                  techTagName: "물주기",
+                },
+                {
+                  techTagId: 3,
+                  techTagName: "병/해충",
+                },
+              ],
+
               areaTag: [
                 {
                   areaTagId: 1,
@@ -91,48 +131,6 @@ export const CaringListHandler = () => {
             },
           ],
         },
-        {
-          data: [
-            {
-              expertProfileId: 1,
-              simpleContent: "식물을 사랑하는 우리 동네 식물 전문가입니다~",
-              address: "연남동",
-              userLikeExpert: 1,
-              member: {
-                memberId: 1,
-                image: {
-                  imageId: 1,
-                  imgUrl: "https://picsum.photos/seed/picsum/200/300",
-                  isRepImg: 1,
-                },
-                name: "김고수",
-                age: 25,
-                gender: 2,
-              },
-
-              techTag: [
-                {
-                  techTagId: 1,
-                  techTagName: "분갈이",
-                },
-                {
-                  techTagId: 2,
-                  techTagName: "물주기",
-                },
-                {
-                  techTagId: 3,
-                  techTagName: "병/해충",
-                },
-              ],
-              areaTag: [
-                {
-                  areaTagId: 1,
-                  ateaTagName: "마포구",
-                },
-              ],
-            },
-          ],
-        }
       )
     );
   });
