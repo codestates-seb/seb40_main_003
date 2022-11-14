380d8984987f9ea9a0f6958a545e313c82ca0f95
Jungu Lee <1zzangjun@gmail.com>
2022-11-14 13:39:32 +0900

    edge case cover

--- client/src/utils/controller.ts
+++ client/src/utils/controller.ts
@@ -9,10 +9,12 @@ export const overKillo =(number:number)=>{
   if(number<=999){
     return number
   }
-  else if(number >999&&number<9999){
+  else if(number>999 && number <= 9999){
     return `${String(number)[0]}천 이상`
   }
-  else return "1만 이상" 
+  else if(number>9999&&number<=99999) {
+    return `${String(number)[0]}만 이상`
+  }
 }
 
 export default isOdd
