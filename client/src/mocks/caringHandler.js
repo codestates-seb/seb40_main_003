80ff28fdc7341481a779b5fa0dd471364b246836
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-14 14:17:11 +0900

    :zap: caringHandler 생성 및 expert-profile-id 추가

--- client/src/mocks/caringHandler.js
+++ client/src/mocks/caringHandler.js
@@ -138,5 +138,114 @@ export const CaringListHandler = ()=>{
       }),
       );
     })
-  
 }
+
+
+rest.get("https://testserver.com/profile-expert", async (req, res, ctx) => {
+  return res(
+    ctx.json(
+      {
+      name: "김고수",
+      age: 25,
+      gender: 2,
+      photos: [
+        {
+          photo: "https://picsum.photos/seed/picsum/200/300​" },
+        { photo: "https://picsum.photos/seed/picsum/200/300​" },
+        { photo: "https://picsum.photos/seed/picsum/400/600​" },
+      ],
+      simpleCotent: "식물을 사랑하는 우리 동네 식물 전문가입니다~",
+      detailContent:
+        "식물자원학과를 졸업하여 전문 지식이 풍부하답니다. 친절하게 이웃 분들의 소중한 반려식물들을 돌보겠습니다.",
+      useNum: 37,
+      price: "기본 요금 30분: 9,000원 1시간 15,000원",
+      extra: "서비스 1개 추가 시 20%씩 할인",
+      address: "연남동",
+      plant: [
+        {
+          plantId: "식물아이디",
+          plantPhoto: "식물사진",
+          plantName: "율무차",
+          plantType: "율마",
+          plantAge: "3년차",
+        },
+      ],
+      expertReview: [
+        {
+          reviewId: "리뷰아이디",
+          content: "너무 친절하게 잘 설명해주시고 소중히 다뤄주셨어요. 짱짱!",
+          writerInfo: {
+            userId: "작성자아이디",
+            userNickname: "초보집사",
+          },
+        },
+      ],
+      techTag: [
+        {
+          techId: "기술태그아이디",
+          name: "분갈이",
+        },
+      ],})
+    
+  );
+})
+
+
+rest.get("https://testserver.com/expert-profile-id", async (req, res, ctx) => {
+  return res(
+    ctx.json(
+      {
+        expertProfileId: 1,
+        simpleContent: "",
+        detailContent: "",
+        useNum: 1,
+        price: 10,
+        address: "",
+        extra: "",
+        userLikeExpert: 1,
+        view: 1,
+        member: {
+            memberId: 1,
+            image: {
+                    imageId: 1,
+                    imgUrl: "",
+                    isRepImg: ""
+                },
+            name: "",
+            age: 1,
+            gender: 1
+        },
+        expertReview: [
+            {
+                expertReviewId: 1,
+                writer: {
+                    memberId: 1,
+                    nickname: ""
+                },
+                content: ""
+            }
+        ],
+        techTag:[
+            {
+                techTagId: 1,
+                techTagName: ""
+            }
+        ],
+        areaTag:[
+            {
+                areaTagId: 1,
+                ateaTagName: ""
+            }
+        ],
+       plant:[
+            {
+                plantId: 1,
+                plantType: "",
+                name: "",
+                year: 1
+            }
+        ]
+    }
+  )
+)}
+)
