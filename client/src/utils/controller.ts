49a8f98f12588776618b90a2183f60158d28b208
Jungu Lee <1zzangjun@gmail.com>
2022-11-14 12:43:36 +0900

    숫자 자리수 줄이기 함수

--- client/src/utils/controller.ts
+++ client/src/utils/controller.ts
@@ -5,5 +5,14 @@ function isOdd(elem:number):boolean {
   return Boolean(elem%2)
 }
 
+export const overKillo =(number:number)=>{
+  if(number<=999){
+    return number
+  }
+  else if(number >999&&number<9999){
+    return `${String(number)[0]}천 이상`
+  }
+  else return "1만 이상" 
+}
 
 export default isOdd
