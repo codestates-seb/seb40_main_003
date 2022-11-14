aae0924e1881b65c0605b49711f32adf84aec00a
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-14 09:14:56 +0900

    :zap: CareCard 스타일 수정

--- client/src/Components/main/CareCard.tsx
+++ client/src/Components/main/CareCard.tsx
@@ -13,26 +13,17 @@ background-color: aliceblue;
   border-bottom: 1px solid var(--line-light-gray);
 `;
 const CareDescription = styled.div`
-background-color: yellow;
+  /* background-color: yellow; */
   width: 100%;
-  height: 100%;
+  height: 100px;
   display: flex;
-  flex-direction: row;
-`;
-const RightWrapper = styled.div`
-background-color: yellowgreen;
-  display: flex;
-  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 `;
 const DescriptionColumnWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
-const Title = styled.span`
-/* background-color: aquamarine; */
-font-size: 15px;
-width: fit-content;
 `;
 
 const CareCard = ({ data }: any) => {
@@ -42,22 +33,24 @@ const CareCard = ({ data }: any) => {
         <>
     <CardWrapper>
       <CareDescription>
-      <ImageWrapper
+        <ImageWrapper
           size={"100"}
           src={data.member.image.imgUrl}
           alt={`상품명 ${data.member.name}의 대표이미지`}
         />
-        </CareDescription>
-        <ColumnWrapper>
-            <span className="medium">{data.member.name} / {data.member.age}세 / {data.member.gender}</span>
-            <span>{data.simpleContent}</span>
-            <span>{data.techTagId}</span>
-            <span className="medium">{data.address}</span>
-            <span>{data.techTag.techTagName}</span>
-        </ColumnWrapper>
         <DescriptionColumnWrapper>
-        <ViewCounter view={data.view} like={data.like} />
-      </DescriptionColumnWrapper>
+          <ColumnWrapper>
+              <span className="medium">{data.member.name} / {data.member.age}세 / {data.member.gender}</span>
+              <span>{data.simpleContent}</span>
+              <span>{data.techTagId}</span>
+              <span className="medium">{data.address}</span>
+              <span>{data.techTag.techTagName}</span>
+          </ColumnWrapper>
+        </DescriptionColumnWrapper>
+        </CareDescription>
+        <DescriptionColumnWrapper>
+          <ViewCounter view={data.view} like={data.like} />
+        </DescriptionColumnWrapper>
     </CardWrapper>
         </>
     );
