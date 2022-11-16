065208405fe52d22a0dc003f64c0fe8c5f5f00d0
Jungu Lee <1zzangjun@gmail.com>
2022-11-16 18:27:18 +0900

    :art: useless component removed

--- client/src/Components/bamboo/BambooCard.tsx
+++ client/src/Components/bamboo/BambooCard.tsx
@@ -1,41 +1,26 @@
-import styled from "@emotion/styled";
-import { BambooImageWrapper, ViewCounter } from "../GlobalComponents";
+import { ImageWrapper, ViewCounter } from "../GlobalComponents";
 import { ColumnWrapper, SectionWrapper } from "../Wrapper";
 import { SpaceEnd } from "../../Components/product/ProductCard";
 
-const BambooWrapper = styled.div`
-  border-bottom: 1px solid var(--line-light-gray);
-  padding: 5px 0;
-`;
-
 const BambooCard = ({ data }: any) => {
   return (
-    <BambooWrapper>
       <ColumnWrapper>
-      <SectionWrapper title={data.title} content={data.content}>
-          {data.image[0]?<BambooImageWrapper
-          size={"100"}
-          src={data.image[0].imgUrl}
-          alt={`상품명 ${data.title}의 대표이미지`}
-        />:<></>}
+        <SectionWrapper title={data.title} content={data.content} width={100}>
+          {data.image[0] ? (
+            <ImageWrapper
+              className="bambooImage mb-16 mt-8"
+              size={"112"}
+              src={data.image[0].imgUrl}
+              alt={`상품명 ${data.title}의 대표이미지`}
+            />
+          ) : (
+            <></>
+          )}
         </SectionWrapper>
         <SpaceEnd>
-          <ViewCounter view={data.view} like={data.likes}/>
+          <ViewCounter view={data.view} like={data.likes} />
         </SpaceEnd>
       </ColumnWrapper>
-      </BambooWrapper>
-
-        // <SectionWrapper title={data.title} content={data.content}>
-        // {data.image[0] ? (
-        //   <BambooImageWrapper
-        //     size={"100"}
-        //     src={data.image[0].imgUrl}
-        //     alt={`상품명 ${data.title}의 대표이미지`}
-        //   />
-        // ):<></>}
-        // </SectionWrapper>
-        //   <ViewCounter view={data.view} like={data.likes} />
-      
   );
 };
 
