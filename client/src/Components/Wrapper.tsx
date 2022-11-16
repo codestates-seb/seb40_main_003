49602994ef1be0dbc8643b804f32e213563d6959
Jungu Lee <1zzangjun@gmail.com>
2022-11-16 14:33:48 +0900

    wrapper

--- client/src/Components/Wrapper.tsx
+++ client/src/Components/Wrapper.tsx
@@ -90,5 +90,12 @@ export const ColumnWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
+export const RowWrapper= styled.div`
+display: flex;
+flex-direction: row;
+&.align-center{
+  align-items: center;
+}
+`;
 
 export default MainLeftWrapper;
