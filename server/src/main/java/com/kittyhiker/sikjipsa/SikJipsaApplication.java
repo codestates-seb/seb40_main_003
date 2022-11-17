9f580cf76f95111862006c1b8df667313f68100a
DasolKang <dasol199@naver.com>
2022-11-17 14:25:42 +0900

    :hammer: ngrok build

--- server/src/main/java/com/kittyhiker/sikjipsa/SikJipsaApplication.java
+++ server/src/main/java/com/kittyhiker/sikjipsa/SikJipsaApplication.java
@@ -2,12 +2,19 @@ package com.kittyhiker.sikjipsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SikJipsaApplication {
+public class SikJipsaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SikJipsaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SikJipsaApplication.class);
 	}
 
 }
