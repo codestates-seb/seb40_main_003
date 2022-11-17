7e9dd62bd95008f1feaa2861b57cddf176eb9def
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-17 16:04:04 +0900

    :zap: Signup page 구성 중

--- client/src/Pages/User/Signup.tsx
+++ client/src/Pages/User/Signup.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from "react-hook-form";
+import { SigButton } from '../../Components/GlobalComponents';
 
 type Props = {}
 
 const Signup = (props: Props) => {
+  const {register, handleSubmit} = useForm();
+  const onValid = () => {
+    console.log("나 발리드됨")
+  };
+  // console.log(register("name"));
+  // console.log(watch());
+  
   return (
-    <div>SignUp</div>
+    <form onSubmit={handleSubmit(onValid)}>
+      <input {...register("username", { required: true })} 
+      type="Text" 
+      placeholder="Username"
+      />
+      <input {...register("email", { required: true })}
+      type="email" 
+      placeholder="Email"
+      />
+      <input {...register("password", { required: true })}
+      type="password" 
+      placeholder="Password"
+      />
+    <SigButton type='submit' value="Create Account"> 계정 생성</SigButton>
+  </form>
   )
+
 }
 
 export default Signup
