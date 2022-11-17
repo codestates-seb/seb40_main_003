cfdaead6f4ce48bc9d79953008ca00649859c91b
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-17 18:03:29 +0900

    :zap: 회원가입 기능 구현 중

--- client/src/Pages/User/Signup.tsx
+++ client/src/Pages/User/Signup.tsx
@@ -1,28 +1,69 @@
-import React from 'react';
-import { useForm } from "react-hook-form";
+import React from 'react';import { FieldErrors, useForm } from "react-hook-form";
 import { SigButton } from '../../Components/GlobalComponents';
 
 type Props = {}
 
+interface SignupForm {
+  username: string;
+  nickname: string;
+  email: string;
+  password: string;
+}
+
 const Signup = (props: Props) => {
-  const {register, handleSubmit} = useForm();
-  const onValid = () => {
+  const {register, handleSubmit, formState: { errors },
+} = useForm<SignupForm>();
+  
+  const onValid = (data: SignupForm) => {
     console.log("나 발리드됨")
+  }
+  const onInValid = (errors: FieldErrors) => {
   };
+
+  console.log(errors);
+  
   // console.log(register("name"));
   // console.log(watch());
   
   return (
-    <form onSubmit={handleSubmit(onValid)}>
-      <input {...register("username", { required: true })} 
-      type="Text" 
-      placeholder="Username"
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
+      <input 
+        {...register("username", { 
+          required: "Username is required",
+          minLength: {
+            message: "이름은 2글자 이상이어야 합니다.",
+            value: 2
+          },
+          maxLength: {
+            message: "이름은 12글자를 넘을 수 없습니다.",
+            value: 12
+          }
+        })} 
+        type="Text" 
+        placeholder="이름"
       />
-      <input {...register("email", { required: true })}
+      {errors.username?.message}
+      <input 
+        {...register("nickname", { 
+          required: "Nickname is required",
+          minLength: {
+            message: "닉네임은 2글자 이상이어야 합니다.",
+            value: 2
+          },
+          maxLength: {
+            message: "닉네임은 12글자를 넘을 수 없습니다.",
+            value: 12
+          }
+        })} 
+        type="Text" 
+        placeholder="닉네임"
+      />
+
+      <input {...register("email", { required: "Email is required" })}
       type="email" 
       placeholder="Email"
       />
-      <input {...register("password", { required: true })}
+      <input {...register("password", { required: "Password is required" })}
       type="password" 
       placeholder="Password"
       />
