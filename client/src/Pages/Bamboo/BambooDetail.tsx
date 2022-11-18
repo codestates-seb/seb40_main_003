ea39dc0525bb58aa949f934a3ce61fdfe2294c17
Jungu Lee <1zzangjun@gmail.com>
2022-11-18 09:46:27 +0900

    :bugs: 타입에러 픽스

--- client/src/Pages/Bamboo/BambooDetail.tsx
+++ client/src/Pages/Bamboo/BambooDetail.tsx
@@ -31,36 +31,31 @@ const BambooDetail = () => {
         setData(res.data);
         setIsLoading(false);
       });
-    } catch (err) {
-    }
+    } catch (err) {}
   }, []);
 
   return !isLoading && data !== null ? (
     <MainContentContainer>
       <MainCenterWrapper>
-        
-      <CommentEdit
-        userId={user}
-        author={data.member.memberId}
-        />
-        <span className='h4 bold font-main mb-16'>{data.title}</span>
+        <CommentEdit userId={user} author={data.member.memberId} />
+        <span className="h4 bold font-main mb-16">{data.title}</span>
         {data.image[0] ? (
           <ImageWrapper
-            className='bambooImage'
+            className="bambooImage"
             size={"360"}
             src={data.image[0].imgUrl}
             alt={`상품명 ${data.title}의 대표이미지`}
           />
         ) : null}
-        <p className='font-black mt-8 text-overflow'>{data.content}</p>
+        <p className="font-black mt-8 text-overflow">{data.content}</p>
         <BambooWrapper>
           <RowWrapper>
-            <span className='sub font-gray'>{data.createdAt}</span>
-            <span className='sub font-gray ml-16'>{data.member.nickname}</span>
+            <span className="sub font-gray">{data.createdAt}</span>
+            <span className="sub font-gray ml-16">{data.member.nickname}</span>
           </RowWrapper>
           <ViewCounter
             view={data.view}
-            renameLike='좋아요'
+            renameLike="좋아요"
             like={data.likeNum}
           />
         </BambooWrapper>
