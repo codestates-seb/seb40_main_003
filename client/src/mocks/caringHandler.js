ceb3afa4ff966c0320fce38c323ef3699c0f91b8
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-18 13:34:58 +0900

    :zap: caringHandler

--- client/src/mocks/caringHandler.js
+++ client/src/mocks/caringHandler.js
@@ -193,51 +193,48 @@ export const CaringListHandler = () => {
   });
 };
 
-export const profileExpertDetail = () => {
-return rest.get("https://testserver.com/profile-expert", async (req, res, ctx) => {
+export const expertProfileHandler = () => {
+return rest.post("https://testserver.com/experts", async (req, res, ctx) => {
   return res(
     ctx.json({
-      name: "김고수",
-      age: 25,
-      gender: 2,
-      photos: [
-        { photo: "https://picsum.photos/seed/picsum/200/300​" },
-        { photo: "https://picsum.photos/seed/picsum/200/300​" },
-        { photo: "https://picsum.photos/seed/picsum/400/600​" },
+      name : "이름",
+      age : "나이",
+      gender : "성별",
+      photo : "프로필사진",
+      simpleCotent : "간단자기소개",
+      detailContent : "상세자기소개",
+      useNum : "고용수",
+      price : "가격",
+      extra : "추가사항",
+      address : "주소",
+      plant : [
+          {
+              plantId : "식물아이디",
+              plantPhoto : "식물사진",
+              plantName : "식물이름",
+              plantType : "식물 종",
+              plantAge : "년수"
+          }
       ],
-      simpleCotent: "식물을 사랑하는 우리 동네 식물 전문가입니다~",
-      detailContent:
-        "식물자원학과를 졸업하여 전문 지식이 풍부하답니다. 친절하게 이웃 분들의 소중한 반려식물들을 돌보겠습니다.",
-      useNum: 37,
-      price: "기본 요금 30분: 9,000원 1시간 15,000원",
-      extra: "서비스 1개 추가 시 20%씩 할인",
-      address: "연남동",
-      plant: [
-        {
-          plantId: "식물아이디",
-          plantPhoto: "식물사진",
-          plantName: "율무차",
-          plantType: "율마",
-          plantAge: "3년차",
-        },
+      expertReview : [
+          {
+              reviewId : "리뷰아이디",
+              content : "리뷰내용",
+              writerInfo : {
+                  userId : "작성자아이디",
+                  userNickname : "작성자닉네임",
+                  createdAt: "작성날짜",
+                  modifiedAt: "수정날짜"
+              }
+          }
       ],
-      expertReview: [
-        {
-          reviewId: "리뷰아이디",
-          content: "너무 친절하게 잘 설명해주시고 소중히 다뤄주셨어요. 짱짱!",
-          writerInfo: {
-            userId: "작성자아이디",
-            userNickname: "초보집사",
-          },
-        },
-      ],
-      techTag: [
-        {
-          techId: "기술태그아이디",
-          name: "분갈이",
-        },
-      ],
-    },
+      techTag : [
+          {
+              techId : "기술태그아이디",
+              name : "기술태그내용"
+          }
+      ]
+  }
     )
   );
 });
