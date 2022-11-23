a11f5df17423823a736a45219c505f3c8ce2ee30
Jungu Lee <1zzangjun@gmail.com>
2022-11-23 09:27:18 +0900

    :bug: return settign page

--- client/src/Pages/settingPage/SettingPage.tsx
+++ client/src/Pages/settingPage/SettingPage.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import { LoadingSpinner } from "../../Components/Loading";
 import {
@@ -8,36 +9,48 @@ import {
 } from "../../Components/Wrapper";
 import useFetch from "../../Hooks/useFetch";
 
+const ContentContainer = styled.div`
+    
+`
+
+const ContentWrapper = styled.div`
+    margin-left: 18px;
+`
+
 const SettingPage = () => {
   // const data = useFetch("/");
   // return data !== undefined ? (
-    return(
+  return (
     <MainContentContainer>
       <MainCenterWrapper>
-        <Link to={"/setting/sales-history"}>
-          <SectionWrapper>
-            <>판매 내역(3)</>
-          </SectionWrapper>
-        </Link>
-        <Link to={"/setting/purchase-history"}>
-          <SectionWrapper>
-            <>구매 내역(3)</>
-          </SectionWrapper>
-        </Link>
-        <Link to={"/setting/caring-history"}>
-          <SectionWrapper>
-            <>돌봄 기록(3)</>
-          </SectionWrapper>
-        </Link>
-        <Link to={"/setting/my-history"}>
-          <SectionWrapper>
-            <>내 게시물(3)</>
-          </SectionWrapper>
-        </Link>
+        <ContentContainer>
+          <ContentWrapper>
+            <Link to={"/setting/sales-history"}>
+              <SectionWrapper>
+                <>판매 내역(3)</>
+              </SectionWrapper>
+            </Link>
+            <Link to={"/setting/purchase-history"}>
+              <SectionWrapper>
+                <>구매 내역(3)</>
+              </SectionWrapper>
+            </Link>
+            <Link to={"/setting/caring-history"}>
+              <SectionWrapper>
+                <>돌봄 기록(3)</>
+              </SectionWrapper>
+            </Link>
+            <Link to={"/setting/my-history"}>
+              <SectionWrapper>
+                <>내 게시물(3)</>
+              </SectionWrapper>
+            </Link>
+          </ContentWrapper>
+        </ContentContainer>
       </MainCenterWrapper>
       <MainRightWrapper></MainRightWrapper>
     </MainContentContainer>
-  )
+  );
   //  : (
   //   <LoadingSpinner />);
 };
