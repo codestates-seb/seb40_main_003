21ec3f9b0113337d2de0c9112e57a760771ffa52
DasolKang <dasol199@naver.com>
2022-11-23 22:36:29 +0900

    :sparkles: deal crud

--- server/src/main/java/com/kittyhiker/sikjipsa/deal/mapper/DealMapper.java
+++ server/src/main/java/com/kittyhiker/sikjipsa/deal/mapper/DealMapper.java
@@ -3,6 +3,7 @@ package com.kittyhiker.sikjipsa.deal.mapper;
 import com.kittyhiker.sikjipsa.deal.dto.DealPostDto;
 import com.kittyhiker.sikjipsa.deal.dto.DealResponseDto;
 import com.kittyhiker.sikjipsa.deal.entity.Deal;
+import com.kittyhiker.sikjipsa.member.dto.MemberResponseDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
@@ -10,6 +11,6 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface DealMapper {
     Deal dealPostDtoToDeal(DealPostDto dealPostDto);
-    DealResponseDto dealToDealResponseDto(Deal deal, List<String> images);
+    DealResponseDto dealToDealResponseDto(Deal deal, List<String> images, MemberResponseDto memberResponse);
 //    List<DealResponseDto> dealListToResponseList(List<Deal> dealList);
 }
