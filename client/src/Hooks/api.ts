2c921d7106821b87ca5016e0211059b279561a9d
wusi-hub <jws9595@naver.com>
2022-11-23 17:12:17 +0900

    :zap: 로그인 전역상태로 변경

--- client/src/Hooks/api.ts
+++ client/src/Hooks/api.ts
@@ -3,8 +3,8 @@ import axios from "../../node_modules/axios/index";
 // axios.defaults.baseURL = "https://fbc9-121-162-186-109.jp.ngrok.io";
 
 // 베이스 URL (기본으로 설정할 백엔드 api 주소)
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL = "https://testserver.com";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = "https://testserver.com";
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "x-CSRFToken";
 
