82da30fbd49ea1ae16b1b13f94c8934c21f38ec6
Jungu Lee <1zzangjun@gmail.com>
2022-11-24 17:22:12 +0900

    :bug: msw error

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -16,6 +16,16 @@ export const handlers = [
         image:
           "https://learnenglish.britishcouncil.org/sites/podcasts/files/2021-10/RS6715_492969113-hig.jpg",
         nickname: "준구짱",
+        accessToken:"이거슨 토큰입니다abcd",
+        refreshToken : "이거슨 리후레시 토큰입니다.abcd",
+      })
+    );
+  }),
+  rest.post("https://testserver.com/users/refresh", async (req, res, ctx) => {
+    return res(
+      ctx.json({
+        accessToken:"이거슨 새로운 토큰입니다abcd",
+        refreshToken : "이거슨 새로운 리후레시 토큰입니다.abcd",
       })
     );
   }),
@@ -24,7 +34,8 @@ export const handlers = [
   }),
   // Handles a GET /user request
 
-  ProductListHandler(),
+  ProductListHandler(1),
+  ProductListHandler(2),
   ProductDetailHandler(1),
   ProductDetailHandler(2),
   ProductDetailHandler(3),
