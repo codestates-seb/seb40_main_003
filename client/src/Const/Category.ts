b9ede7c12a50ef7b214c36e448b999532572ef57
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-11-25 09:48:26 +0900

    :sparkles: 돌봄/거래 카테고리 배열 선언

---
+++ client/src/Const/Category.ts
@@ -0,0 +1,24 @@
+export const ProductCategoryName = [
+    {number:1, name: "대형 식물(100cm 내외)"},
+    {number:1, name: "중형 식물(50cm 내외)"},
+    {number:1, name: "소형 식물(30cm 이하)"},
+    {number:1, name: "씨앗/모종"},
+    {number:1, name: "화분/화병"},
+    {number:1, name: "가구/장식"},
+    {number:1, name: "도구/용품"},
+    {number:1, name: "기계"},
+    {number:1, name: "서적"}
+]
+
+export const CareCategoryName = [
+    {number:1, name: "물 주기"},
+    {number:1, name: "분갈이"},
+    {number:1, name: "화병 관리"},
+    {number:1, name: "수형 관리"},
+    {number:1, name: "잎 솎기"},
+    {number:1, name: "잎 닦기"},
+    {number:1, name: "가지치기"},
+    {number:1, name: "병/해충"},
+    {number:1, name: "기타"}
+]
+
