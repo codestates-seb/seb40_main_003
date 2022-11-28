dc8c0beed67abd46b281a853edfb0d867c76b246
DasolKang <dasol199@naver.com>
2022-11-28 16:42:23 +0900

    :bug: build error

--- server/src/main/java/com/kittyhiker/sikjipsa/caring/mapper/ExpertMapper.java
+++ server/src/main/java/com/kittyhiker/sikjipsa/caring/mapper/ExpertMapper.java
@@ -3,7 +3,6 @@ package com.kittyhiker.sikjipsa.caring.mapper;
 import com.kittyhiker.sikjipsa.caring.dto.ExpertProfileDto;
 import com.kittyhiker.sikjipsa.caring.dto.ExpertProfileResponseDto;
 import com.kittyhiker.sikjipsa.caring.dto.ExpertProfileResponseDtos;
-import com.kittyhiker.sikjipsa.caring.dto.ExpertSuccessResponseDtos;
 import com.kittyhiker.sikjipsa.caring.entity.ExpertProfile;
 import org.mapstruct.Mapper;
 
