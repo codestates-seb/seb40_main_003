feecf5b8852bcb476bb3053ce16c4dccd4dec0a3
Jungu Lee <1zzangjun@gmail.com>
2022-11-30 02:23:11 +0900

    :art: message fixed

--- client/src/Const/message.ts
+++ client/src/Const/message.ts
@@ -3,3 +3,7 @@ export const cannotLoad = "컨텐츠를 불러오지 못했습니다" as const
 export const searchbarComment = "궁금한게 있다면? 검색해보세요" as const
 
 export const noExistPage ="존재하지 않는 페이지입니다" as const
+
+export const confirmCancel = "작성을 취소하시겠습니까?" as const
+
+export const confirmLogout = "로그아웃 하시겠습니까?" as const
