374a7b5484d0b1ebaad4378e099d96947eb031f1
herong0515 <102296873+herong0515@users.noreply.github.com>
2022-12-01 22:16:39 +0900

    :sparkles: 메인 페이지 배너 텍스트 추가 중

--- client/src/Pages/Main/Main.tsx
+++ client/src/Pages/Main/Main.tsx
@@ -8,6 +8,8 @@ import {
 import MainBanner1 from '../../images/banner/mainBanner1.png'
 import MainBanner2 from '../../images/banner/mainBanner2.png'
 import MainBanner3 from '../../images/banner/mainBanner3.png'
+import MainBanner4 from '../../images/banner/mainBanner4.png'
+
 import { axiosPrivate } from "../../Hooks/api";
 
 const Main = () => {
@@ -15,16 +17,31 @@ const Main = () => {
     <MainContentContainer>
       <MainCenterWrapper className="pd-0">
         <Carousel showStatus={false} autoPlay={true} infiniteLoop={true} showThumbs={false}>
-          <BannerWrapper image={MainBanner1}/>
-          <BannerWrapper image={MainBanner2}/>
-          <BannerWrapper image={MainBanner3}/>
+          <BannerWrapper>
+            <BannerImgWrapper image={MainBanner1}>
+              <BannerTextLeft>
+                <p className="h4">먹이고~ 볕쬐고~ 애지중지 키운</p>
+                <p className="h1">반려식물 자랑하기!</p>
+              </BannerTextLeft>
+            </BannerImgWrapper>
+          </BannerWrapper>
+          <BannerImgWrapper image={MainBanner2}></BannerImgWrapper>
+          <BannerImgWrapper image={MainBanner3}></BannerImgWrapper>
+          <BannerImgWrapper image={MainBanner4}></BannerImgWrapper>
+
         </Carousel>
       </MainCenterWrapper>
       <MainRightWrapper></MainRightWrapper>
     </MainContentContainer>
   );
 };
+
 const BannerWrapper = styled.div`
+  width: 100%;
+  margin: 10px auto;
+  position: relative;
+`
+const BannerImgWrapper = styled.div`
   background-image: ${(props:{image:string})=> `url(${props.image})`};
   width: 100%;
   height: 30vw;
@@ -34,4 +51,25 @@ const BannerWrapper = styled.div`
   background-position: center;
   background-size:cover;
 `
+const BannerTextLeft = styled.div`
+  background-color: aqua;
+  position: absolute;
+  top: 40%;
+  left: 40%;
+  width: 50%;
+  transform: translate(-50%, -50%);
+  font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+`
+const BannerTextRight = styled.div`
+  background-color: aqua;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  width: 50%;
+  transform: translate(-50%, -50%);
+  font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+`
+
 export default Main;
