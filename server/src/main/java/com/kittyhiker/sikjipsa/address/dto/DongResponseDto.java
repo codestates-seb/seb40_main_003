ddf197727740331c83afa91e09ccf223b4d9d67d
NUCY <nucystates@gmail.com>
2022-12-02 15:40:16 +0900

    :sparkles: [Feat] Address search

---
+++ server/src/main/java/com/kittyhiker/sikjipsa/address/dto/DongResponseDto.java
@@ -0,0 +1,10 @@
+package com.kittyhiker.sikjipsa.address.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DongResponseDto {
+	private String dong;
+}
